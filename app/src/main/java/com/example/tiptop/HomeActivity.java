142a0cdbf347b3a9101de823cac59ca09e24dcf2
TomLatin <tomlatin52@gmail.com>
2020-11-26 12:18:40 +0200

    Adding the classes and XML related to home activity

--- app/src/main/java/com/example/tiptop/HomeActivity.java
+++ app/src/main/java/com/example/tiptop/HomeActivity.java
@@ -1,15 +1,120 @@
 package com.example.tiptop;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+
+    //Variables that will contain all the buttons
+    private Button setting = null;
+    private Button profile = null;
+    private ImageButton  imageButton;
+    private Button followUp = null;
+    private Button Statistics = null;
+    private Button chat = null;
+    private Button Tasks = null;
+    private Button history = null;
+    private Button points = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //Set the settings button
+        setting = (Button)findViewById(R.id.setting);
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(),SettingActivity.class);
+                startActivity(i);
+            }
+        });
+
+        //Set the profile button
+        profile = (Button)findViewById(R.id.profile);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(),ProfileActivity.class);
+                startActivity(i);
+            }
+        });
+
+        //Set the followUp button
+        followUp = (Button)findViewById(R.id.followUp);
+        followUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(),FollowUpActivity.class);
+                startActivity(i);
+            }
+        });
+
+        //Set the Statistics button
+        Statistics = (Button)findViewById(R.id.statistics);
+        Statistics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(),StatisticsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        //Set the chat button
+        chat = (Button)findViewById(R.id.chat);
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(),ChatActivity.class);
+                startActivity(i);
+            }
+        });
+
+        //Set the Tasks button
+        Tasks = (Button)findViewById(R.id.tasks);
+        Tasks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(),TasksActivity.class);
+                startActivity(i);
+            }
+        });
+
+        //Set the history button
+        history = (Button)findViewById(R.id.history);
+        history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(),HistoryActivity.class);
+                startActivity(i);
+            }
+        });
+
+        //Set the points button
+        points = (Button)findViewById(R.id.points);
+        points.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(),PointsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        //Set the ImageButton button
+        imageButton = (ImageButton)findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), ImageActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
