a9b9e2c824f9375028ffe7ef34669049172f2cf2
yiratpeleg <57325378+yiratpeleg@users.noreply.github.com>
2020-12-01 19:22:05 +0200

    change the logoActivity

--- app/src/main/java/com/example/tiptop/LogoActivity.java
+++ app/src/main/java/com/example/tiptop/LogoActivity.java
@@ -4,24 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LogoActivity extends AppCompatActivity {
 
-    private Button login = null;
+    private static int SPLASH_SCREEN = 3000;
+
+    private TextView logo_text;
+    private ImageView logo_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_logo);
-        login = (Button)findViewById(R.id.login);
-        login.setOnClickListener(new View.OnClickListener() {
+
+        logo_text = (TextView) findViewById(R.id.logo_text);
+        logo_image = (ImageView) findViewById(R.id.logo_image);
+
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent i = new Intent(v.getContext(),LoginActivity.class);
-                startActivity(i);
+            public void run() {
+                Intent go_to_login = new Intent(LogoActivity.this,LoginActivity.class);
+                startActivity(go_to_login);
+                finish();
             }
-        });
+        },SPLASH_SCREEN);
     }
 }
