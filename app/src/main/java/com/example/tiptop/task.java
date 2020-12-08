a86085144f243162f663193a63eb4e17cea19b6a
TomLatin <tomlatin52@gmail.com>
2020-12-08 09:24:09 +0200

    Add Class task

---
+++ app/src/main/java/com/example/tiptop/task.java
@@ -0,0 +1,83 @@
+package com.example.tiptop;
+
+import java.io.Serializable;
+import java.time.LocalDateTime; // import the LocalDateTime class
+import com.google.firebase.storage.StorageReference;
+
+public class task implements Serializable {
+
+    private String nameTask;
+    private int bonusScore;
+    private String  belongsToUID; //Here we enter the UID from the function firebase.auth().Currentuser.uid
+    private  LocalDateTime startDateAndHour;
+    private  LocalDateTime endDateAndHour;
+    private int status; //when 0-Not associated, 1 - Associated, 2 - Waiting for approval, 3- Confirmed, 4 - Not confirmed.
+    private String comment;
+    private StorageReference image;
+
+    //Getter
+    public String getNameTask() {
+        return nameTask;
+    }
+
+    public int getBonusScore() {
+        return bonusScore;
+    }
+
+    public String getBelongsToUID() {
+        return belongsToUID;
+    }
+
+    public LocalDateTime getStartDateAndHour() {
+        return startDateAndHour;
+    }
+
+    public LocalDateTime getEndDateAndHour() {
+        return endDateAndHour;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public StorageReference getImage() {
+        return image;
+    }
+
+    //Setter
+    public void setNameTask(String nameTask) {
+        this.nameTask = nameTask;
+    }
+
+    public void setBonusScore(int bonusScore) {
+        this.bonusScore = bonusScore;
+    }
+
+    public void setBelongsToUID(String belongsToUID) {
+        this.belongsToUID = belongsToUID;
+    }
+
+    public void setStartDateAndHour(LocalDateTime startDateAndHour) {
+        this.startDateAndHour = startDateAndHour;
+    }
+
+    public void setEndDateAndHour(LocalDateTime endDateAndHour) {
+        this.endDateAndHour = endDateAndHour;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public void setImage(StorageReference image) {
+        this.image = image;
+    }
+}
