527713360c1176708175aba9c5b7c101325f0404
TomLatin <tomlatin52@gmail.com>
2020-12-09 23:30:02 +0200

    add spiner to home

---
+++ app/src/main/java/com/example/tiptop/TasksParentActivity.java
@@ -0,0 +1,34 @@
+package com.example.tiptop;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class TasksParentActivity extends AppCompatActivity {
+
+    private ListView ListOfTasksNotAssociated;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase db;
+    private DatabaseReference reference;
+    private  String uid;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_parent);
+        initializeClassVariables();
+
+    }
+
+    private void initializeClassVariables() {
+        ListOfTasksNotAssociated =(ListView) findViewById(R.id.ListOfTasksNotAssociated);
+        mAuth = FirebaseAuth.getInstance();
+        uid = mAuth.getCurrentUser().getUid();
+    }
+}
